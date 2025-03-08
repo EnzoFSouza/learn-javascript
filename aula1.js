aa489ba34e2f462afbe029ffb244c9438d8143fc
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-08 13:33:57 -0300

    aula 2

---
+++ aula1.js
@@ -0,0 +1,4 @@
+console.log("Hello World!");
+alert("Hello World!");
+confirm("Confirme para continuar");
+prompt("Digite seu nome: ");
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-console.log("Hello World!");
-alert("Hello World!");
-confirm("Confirme para continuar");
-prompt("Digite seu nome: ");
+var a = 10;
+let b = 20;
+const c = 30;
+
+a = 15;
+c = 35; //não pode mudar pois é constante
+console.log(c);
