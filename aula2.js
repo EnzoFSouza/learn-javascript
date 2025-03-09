0d46b0f4063e6628cc1f15d9dacaed753db568ce
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-09 14:25:16 -0300

    aula sobre tipos de variaveis está no script.js

---
+++ aula2.js
@@ -0,0 +1,7 @@
+var a = 10;
+let b = 20;
+const c = 30;
+
+a = 15;
+c = 35; //não pode mudar pois é constante
+console.log(c);
--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var a = 10;
-let b = 20;
-const c = 30;
+let b = 20.5;
+const c = "30 anos";
+let d = true;
+let e = null;
+let f = undefined;
 
-a = 15;
-c = 35; //não pode mudar pois é constante
-console.log(c);
+/*console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)*/
+console.log(a+b)
