53e0d44c7efee766bcc84d4696a590b444f59a6e
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-11 14:43:09 -0300

    aula sobre strings_

--- script.js
+++
@@ -1,9 +0,0 @@
-let a = "Nome Sobrenome"
-    //   0123456789
-console.log(a.length)
-console.log(a.slice(5))
-console.log(a.substring(3, 6))
-console.log(a.toUpperCase())
-console.log(a.toLowerCase())
-
-console.log(a.at(7))
