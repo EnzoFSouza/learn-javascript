368f97919c23f1bd5e7da6646aeeabb4933d24c9
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-12 14:16:25 -0300

    aula sobre comparações

---
+++ aula6.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 2;
+let c = 5.0;
+let d = "5";
+
+console.log(a == b);
+console.log(a == c);
+console.log(a != c);
+console.log(a == d);
+console.log(a === c);
+console.log(a === d);
+
+console.log(a > b);
+console.log(a < b);
+console.log(a <= b);
