bfc4c63cc94eecb8f2ffb9b1bd0a65b3f8a6676e
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-14 15:03:17 -0300

    aula 8 operadores condicionais

---
+++ operadores_condicionais.js
@@ -0,0 +1,13 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+
+console.log(a && b);
+console.log(a && c);
+console.log(a || b);
+console.log(a || c);
+console.log(b || d);
+console.log(!a);
+console.log(!b);
+console.log(!d);
